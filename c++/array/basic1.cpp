3b50d424cf74bc192a9604d2d6c2cca5c001225e
rain-kun <rainkun3@protonmail.com>
2021-08-12 01:21:42 +0530

    refractered

--- c++/array/basic1.cpp
+++ c++/array/basic1.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 
+// 1)Create an Array of size 10 of integers. Take input from the user for these 10 elements and print the entire array after that.
 void question1(){
     int arr[10];
     cout<<"give elements"<<endl;
@@ -14,6 +15,7 @@ void question1(){
     }
 }
 
+// 2)Check whether n is present in an array of size m or not.
 int question2(){
     vector<int> arr;int ele, si;
     cin>>ele>>si;
@@ -30,6 +32,7 @@ int question2(){
     return 0;
 }
 
+// 3)Find the minimum and maximum element in an array.
 void question3(){
     vector<int>arr;
     int min, max, si;
@@ -49,6 +52,7 @@ void question3(){
     cout<<"min:"<<min<<"\n"<<"max:"<<max<<endl;
 }
 
+// 4)Write a program to reverse the array.
 void question4(){
     vector<int>arr;
     int si;
@@ -97,6 +101,7 @@ void question4(){
 //     cout<<"min:"<<min<<"\n"<<"max:"<<max<<endl;
 // }
 
+// 6)Find the Kth largest and Kth smallest number in an array.
 void question6(){
     int k, si;
     cin>>k>>si;
@@ -119,36 +124,103 @@ void question6(){
 	cout << *itrr << "\n";
 }
 
-// void question8(){
-//     int si;
-//     cin>>si;
-//     vector<int> arr;
-//     arr.assign(si, 0);
-//     // cout<<arr.size();
-//     for(auto i=arr.begin(); i != arr.end(); i++){
-//         cin>>*i;
-//     }
-//     auto f = arr.begin(), c = arr.begin();
-//     while(f!=arr.end()){
-//         while(c!=arr.end()){
-//             if (*f == *c){
-//                 c +=1;
-//             }
-//             if (*f > *c){
-//                int temp = *f;*f=*c;*c=temp;
-//                 f +=1;
-//             }
-//             c +=1;
-//         }
-//         c = f;
-//         cout << f << c << endl;
-//     }
-//     cout<<"ans"<<endl;
-//     for(auto i=arr.begin(); i != arr.end(); i++){
-//         cout<<*i;
-//     }
+// 8)Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algorithm.
+void question8(){
+    int si;
+    cin>>si;
+    int arr[si];
+    int l = sizeof(arr)/sizeof(arr[0]);
+    // arr.assign(si, 0);
+    // cout<<arr.size();
+    for(auto i=0; i < l; i++){
+        cin>>arr[i];
+    }
+    // while(si--){
+    for (int* i=arr; i<arr+l; i++){
+        for (int* j=i; j<arr+l; j++){
+            if(*i > *j){
+                cout<<*i<<*j<<endl;
+                int temp = *i;*i=*j;*j=temp;
+                cout<<*i<<*j<<endl;
+            }
+        }
 
-// }
+    }
+    cout<<"ans"<<endl;
+    for(auto i=0; i < l; i++){
+        cout<<arr[i];
+    }
+    // while(*f!=arr){
+    //     while(c!=arr.end()){
+    //         if (*f == *c){
+    //             c +=1;
+    //         }
+    //         if (*f > *c){
+    //            int temp = *f;*f=*c;*c=temp;
+    //             f +=1;
+    //         }
+    //         c +=1;
+    //     }
+    //     c = f;
+    //     cout << f << c << endl;
+    // }
+    // cout<<"ans"<<endl;
+    // for(auto i=arr.begin(); i != arr.end(); i++){
+    //     cout<<*i;
+    // }
+
+}
+
+// 9)Find the range of the array. Range means the difference between the maximum and minimum element in the array.
+void question9(){
+    vector<int>arr;
+    int min, max, si;
+    cin>>si;
+    for(int i=0; i<si; i++){
+        int a;cin>>a;arr.push_back(a);
+    }
+    min = max = *arr.begin();
+    for(auto i=arr.begin(); i != arr.end(); i++){
+        if(*i <= min){
+            min = *i;
+        }
+        if(*i >= max){
+            max = *i;
+        }
+    }
+    cout<<"diff:"<< max-min <<endl;
+}
+
+// 10) Move all the negative elements to one side of the array.
+void question10(){
+     int si;
+    cin>>si;
+    int arr[si];
+    int l = sizeof(arr)/sizeof(arr[0]);
+    // arr.assign(si, 0);
+    // cout<<arr.size();
+    for(auto i=0; i < l; i++){
+        cin>>arr[i];
+    }
+    // while(si--){
+    for (int* i=arr; i<arr+l; i++){
+        for (int* j=i; j<arr+l; j++){
+            if(*i < 0){
+                break;
+            }
+            if (*i > 0 && *j < 0){
+                int temp = *i;*i=*j;*j=temp;
+                break;
+            }
+        }
+
+    }
+    cout<<"ans"<<endl;
+    for(auto i=0; i < l; i++){
+        cout<<arr[i];
+    }
+}
+
 int main(){
     // question1();
     // question2();
@@ -156,6 +228,8 @@ int main(){
     // question4();
     // question6();
     // question8();
+    // question9();
+    question10();
 
     return 0;
 }
