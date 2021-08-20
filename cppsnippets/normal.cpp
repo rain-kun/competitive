6009cb4839978cf86beadb556ed610b7881041a4
rain-kun <rainkun3@protonmail.com>
2021-08-20 11:08:10 +0530

    cpp snippets

---
+++ cppsnippets/normal.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pb                      push_back
+#define pf                      push_front
+#define make                    make_pair
+
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);cout.tie(0);
+
+
+  return 0;
+}
